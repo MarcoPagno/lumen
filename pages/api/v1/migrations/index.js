93117b44d87fdb29d83a8a094e0a4d5b78e582b2
Marco Pagno <marcoapg@live.com>
2026-04-21 20:19:20 -0300

    feat: migrations implemented, with api endpoint and tests

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,60 @@
+import { createRouter } from "next-connect";
+import { runner } from "node-pg-migrate";
+import { resolve } from "node:path";
+import database from "infra/database.js";
+import controller from "infra/controller.js";
+
+const router = createRouter();
+
+router.get(getHandler);
+
+router.post(postHandler);
+
+export default router.handler(controller.errorHandlers);
+
+let defaultMigrationOptions = {
+  databaseUrl: process.env.DATABASE_URL,
+  dryRun: true,
+  dir: resolve(process.cwd(), "infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
+
+async function getHandler(request, response) {
+  let dbClient;
+
+  try {
+    dbClient = await database.getNewClient();
+
+    const pendingMigrations = await runner({
+      ...defaultMigrationOptions,
+      dbClient,
+    });
+
+    return response.status(200).json(pendingMigrations);
+  } finally {
+    await dbClient.end();
+  }
+}
+
+async function postHandler(request, response) {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+
+    const migratedMigrations = await runner({
+      ...defaultMigrationOptions,
+      dbClient,
+      dryRun: false,
+    });
+
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    }
+
+    return response.status(200).json(migratedMigrations);
+  } finally {
+    await dbClient.end();
+  }
+}
